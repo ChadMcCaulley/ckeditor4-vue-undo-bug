84438733ef3fa54ca243fa0deb239d1cf652e4c2
ChadM <chadmccaulley95@gmail.com>
2022-12-06 15:01:02 -0700

    [WIP] Adds ckeditor config

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ckeditor v-model="editor1Value" :config="editorConfig"></ckeditor>
+    <ckeditor v-model="editor2Value" :config="editor2Config"></ckeditor>
+  </div>
 </template>
+
+// If you want to use this template, please follow the following steps: // 1.
+Fork it. // 2. Add your code here, e.g. custom plugins. // 3. If you want to
+change the default ckeditor4-vue version, you can do it in the sidebar on the
+left, inside "Dependencies" section via dropdown.
 
 <script>
 export default {
-  name: 'App',
+  name: "App.vue",
+  data: () => ({
+    editor1Value: "",
+    editor2Value: "",
+    editorConfig: {
+      removePlugins: "elementspath,exportpdf,magicline,resize,justify",
+      extraPlugins: ["divarea", "stylesheetparser", "sourcearea"],
+      toolbar: [
+        {
+          items: [
+            "Source",
+            "-",
+            "Bold",
+            "Italic",
+            "RemoveFormat",
+            "-",
+            "SpecialChar",
+          ],
+        },
+      ],
+    },
+    editor2Config: {
+      removePlugins: "elementspath,exportpdf,magicline,resize,justify",
+      extraPlugins: ["divarea", "stylesheetparser", "sourcearea"],
+      toolbar: [
+        {
+          items: [
+            "Source",
+            "-",
+            "Bold",
+            "Italic",
+            "RemoveFormat",
+            "-",
+            "SpecialChar",
+          ],
+        },
+      ],
+    },
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
